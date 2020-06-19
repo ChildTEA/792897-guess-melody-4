d61af9a89344bc1a3f6b03f32043ceb3c4c83370
Yaroslav Smirnov <yar14@mail.ru>
2020-06-19 21:38:37 +0300

    Feat: Реализована логика смены экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,24 +1,73 @@
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import PropTypes from "prop-types";
 import React, {PureComponent} from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
-import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
 
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      step: -1,
+    };
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0,
+            });
+          }}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
+
+    return null;
   }
 
   render() {
-    const {errorsCount} = this.props;
-
-    return (
-      <WelcomeScreen
-        errorsCount={errorsCount}
-        onWelcomeButtonClick={() => {}}
-      />
-    );
+    return this._renderGameScreen();
   }
 }
+
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
