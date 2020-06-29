c3908735942fe33cb2c87686f5e8584f9a342af7
Yaroslav Smirnov <yar14@mail.ru>
2020-06-29 21:15:29 +0300

    Feat: store подключен в приложение (в index.js)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer.js";
 import {questions} from "./mocks/questions.js";
 
+
+const store = createStore(reducer);
 
 const Settings = {
   ERRORS_COUNT: 3,
@@ -10,9 +15,11 @@ const Settings = {
 
 
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
